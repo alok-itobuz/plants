7c86aaec7e45dc4e68eb24d0a0d9e320e60ff16e
Alok905 <alokranjanjoshi07567@gmail.com>
2024-03-06 00:08:51 +0530

    so much done

--- scripts/main.js
+++ scripts/main.js
@@ -17,8 +17,7 @@ const collectionCarouselButtonsContainer = document.querySelector(
 
 const createCarouselCard = (i) => {
   return `
-    <div class="home-carousel-item w-100 h-100 position-absolute ${
-      i == 0 ? "active" : ""
+    <div class="home-carousel-item w-100 h-100 position-absolute ${i == 0 ? "active" : ""
     }" data-id=${i}>
         <div class="home-carousel-body position-relative h-100 w-100 overflow-hidden ">
             <img class="" src="../assets/static/home/plant_${i}.png" alt="home plant">
@@ -27,9 +26,8 @@ const createCarouselCard = (i) => {
     </div>`;
 };
 function createSliderSpan(i) {
-  return `<span class="home-carousel-slider-span h-100  border-0 ${
-    i == 0 ? "active" : ""
-  }" data-id=${i}>
+  return `<span class="home-carousel-slider-span h-100  border-0 ${i == 0 ? "active" : ""
+    }" data-id=${i}>
     </span>`;
 }
 function createCollectionCarouselCard(i) {
@@ -37,18 +35,56 @@ function createCollectionCarouselCard(i) {
     <div class="collection-carousel-item h-100 w-100 position-absolute p-1 p-md-2 p-lg-3 " data-id=${i}>
         <div class="carousel-item-body h-100 w-100 d-flex flex-column">
             <div class="carousel-image-container flex-grow-1 overflow-hidden position-relative">
-                <img class="object-fit-cover " src="../assets/static/collection/plant_${
-                  i % 3
-                }.svg" alt="plant">
-                <img class="object-fit-cover position-absolute start-0 top-0 w-100 h-100" src="../assets/static/collection/wrap_plant_${
-                  i % 3
-                }.png" alt="plant">
+                <img class="object-fit-cover " src="../assets/static/collection/plant_${i % 3
+    }.svg" alt="plant">
+                <img class="object-fit-cover position-absolute start-0 top-0 w-100 h-100" src="../assets/static/collection/wrap_plant_${i % 3
+    }.png" alt="plant">
             </div>
             <h4 class="fw-semibold m-0 d-flex align-items-center ">Bird of paradise
             </h4>
         </div>
     </div>
     `;
+}
+function createClientButtonWrapper() {
+  return `
+    <div class="clients-button-wrapper d-none d-md-flex flex-column align-items-center justify-content-center">
+        <button
+            class="client-carousel-button d-none d-md-flex justify-content-center align-items-center rounded-circle">
+            <img class="h-100 w-100" src="../assets/static/clients/arrow_right.svg" alt="right arrow" />
+        </button>
+    </div>
+  `
+}
+function createClientCard(i) {
+  return `
+    <div
+        class="client-carousel-item ${i === 0 ? 'active' : ''} h-100 w-100 position-relative d-flex flex-column flex-md-row justify-content-around align-items-center p-4">
+        <div class="client-image rounded-circle overflow-hidden">
+            <img class="h-100 w-100" src="../assets/static/clients/client_1.svg" alt="" />
+        </div>
+        <div class="client-description ${i === 0 ? 'active' : ''} d-flex flex-column justify-content-center position-relative w-100">
+            <span class="d-inline-flex align-items-center justify-content-center  double-quotes"><img
+                                class="h-100 w-100 " src="../assets/static/clients/double_quotes.png" alt=""></span>
+            <p class="fw-normal m-0">
+                1It is a long established fact that a reader will be distracted
+                by the readable content of a page when It is a long .It is a
+                long .It is a long established fact that a reader will be . It
+                is a long established fact that a reader .
+            </p>
+            <span class="d-inline-flex align-items-center justify-content-center  double-quotes position-absolute"><img
+                                class="h-100 w-100 " src="../assets/static/clients/double_quotes.png" alt=""></span>
+        </div>
+        <button
+            class="prev client-carousel-button d-flex d-md-none position-absolute justify-content-center align-items-center rounded-circle">
+            <img src="../assets/static/clients/arrow_right.svg" alt="right arrow" />
+        </button>
+        <button
+            class="next client-carousel-button d-flex d-md-none position-absolute justify-content-center align-items-center rounded-circle">
+            <img src="../assets/static/clients/arrow_right.svg" alt="right arrow" />
+        </button>
+    </div>
+  `
 }
 
 // change slide according to the current page
@@ -71,11 +107,8 @@ const displayCarouselItem = (
   for (let i = 0; i < totalCarouselItem; i++) {
     const currentItem = carouselItems[i];
 
-    currentItem.style.translate = `${
-      (+currentItem.dataset.id - currentCarouselItem) * translateFactor
-    }% ${translateY ? "-50%" : ""}`;
-    // currentItem.style.transform = `translateX(${(+currentItem.dataset.id - currentCarouselItem) * translateFactor
-    //     }%) ${translateY ? "translateY(-50%)" : ""}`;
+    currentItem.style.translate = `${(+currentItem.dataset.id - currentCarouselItem) * translateFactor
+      }% ${translateY ? "-50%" : ""}`;
 
     currentItem.classList.remove("active");
 
@@ -191,15 +224,19 @@ collectionCarouselButtonsContainer.addEventListener("click", (e) => {
 //////////Client Carousel///////////////
 let currentClientCarouselItem = 0;
 const totalClientCarousalItem = 3;
-const carouselMediumButtons = document.querySelectorAll(
-  ".clients-button-wrapper button"
-);
-console.log(carouselMediumButtons);
+
 const clientCarouselContainer = document.querySelector(
   ".clients-carousel-container"
 );
 const clientCarouselWrapper = document.querySelector(
   ".clients-carousel-container>.carousel-wrapper"
+);
+for (let i = 0; i < totalClientCarousalItem; i++) {
+  if (i > 0) clientCarouselWrapper.insertAdjacentHTML('beforeend', createClientButtonWrapper())
+  clientCarouselWrapper.insertAdjacentHTML('beforeend', createClientCard(i))
+}
+const carouselMediumButtons = document.querySelectorAll(
+  ".clients-button-wrapper button"
 );
 
 let isInitialDone = false,
@@ -221,11 +258,10 @@ clientCarouselContainer.addEventListener("click", (e) => {
 
   if (isMedium) {
     if (!currentClientCarouselItem) {
-      console.log(currentClientCarouselItem);
       translationFactor = 0;
     } else translationFactor = 45 + (currentClientCarouselItem - 1) * 65;
 
-    console.log(currentClientCarouselItem);
+
     carouselMediumButtons.forEach((el, i) => {
       if (i < currentClientCarouselItem) {
         el.classList.remove("next");
@@ -238,6 +274,18 @@ clientCarouselContainer.addEventListener("click", (e) => {
   }
 
   clientCarouselWrapper.style.transform = `translateX(-${translationFactor}%)`;
+  const items = clientCarouselWrapper.querySelectorAll('.client-carousel-item')
+  const descriptions = clientCarouselWrapper.querySelectorAll('.client-carousel-item .client-description')
+  items.forEach((item, i) => {
+    if (i === currentClientCarouselItem) {
+      item.classList.add('active')
+      descriptions[i].classList.add('active')
+    }
+    else {
+      item.classList.remove('active')
+      descriptions[i].classList.remove('active')
+    }
+  })
 });
 // IIFE
 (function () {
